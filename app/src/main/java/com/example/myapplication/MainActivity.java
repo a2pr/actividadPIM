2299b783ec5390c829af3deff56b44e845468280
Andres <a2payema@gmail.com>
2020-10-24 19:21:22 -0400

    Cadastro layout

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onCadastrar(View view){
+        EditText newLogin = (EditText)findViewById(R.id.newLogin);
+        EditText newSenha = (EditText)findViewById(R.id.newSenha);
+        EditText newNome = (EditText)findViewById(R.id.newNome);
+
+
     }
 }
