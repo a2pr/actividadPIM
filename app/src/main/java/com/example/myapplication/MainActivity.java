1d4cb444849a9916fcdafbdf84b24a4f68ea70be
Andres <a2payema@gmail.com>
2020-10-25 18:17:26 -0400

    new layouts

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +18,32 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void onCadastrar(View view){
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.xml.menu, menu );
+        return true;
+    }
+
+    public boolean onOptionsItemSelected( MenuItem item){
+        switch ( item.getItemId()){
+            case R.id.novo:
+                // open new intent.
+                Intent intent = new Intent(this, Cadastro.class);
+                startActivity(intent);
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void onLogin(View view){
+
+        Intent intent = new Intent(this, UsuarioInfo.class);
         EditText newLogin = (EditText)findViewById(R.id.newLogin);
         EditText newSenha = (EditText)findViewById(R.id.newSenha);
-        EditText newNome = (EditText)findViewById(R.id.newNome);
 
-
+        /**
+         here we get the user
+         **/
+        Toast.makeText(this, "login made", Toast.LENGTH_SHORT).show();
+        startActivity(intent);
     }
 }
