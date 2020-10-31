37e5a74523e88f0d45dc5758c411d45d3e2181a1
Andres <a2payema@gmail.com>
2020-10-31 16:01:05 -0400

    add new img, and new activity

--- app/src/main/java/com/example/myapplication/home.java
+++ app/src/main/java/com/example/myapplication/home.java
@@ -7,43 +7,34 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class home extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
-        getMenuInflater().inflate(R.xml.menu, menu );
+        getMenuInflater().inflate(R.xml.menu_home, menu );
         return true;
     }
 
     public boolean onOptionsItemSelected( MenuItem item){
         switch ( item.getItemId()){
-            case R.id.novo:
+            case R.id.info:
                 // open new intent.
-                Intent intent = new Intent(this, Cadastro.class);
+                Intent intent = new Intent(this, UsuarioInfo.class);
                 startActivity(intent);
+
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    public void onLogin(View view){
-
-        Intent intent = new Intent(this, home.class);
-        EditText newLogin = (EditText)findViewById(R.id.newLogin);
-        EditText newSenha = (EditText)findViewById(R.id.newSenha);
-
-        /**
-         here we get the user
-         **/
-        Toast.makeText(this, "login made", Toast.LENGTH_SHORT).show();
-        startActivity(intent);
+    public void buyIngresso(View view){
+        Toast.makeText(this, "Comprando ingresso", Toast.LENGTH_SHORT).show();
     }
 }
